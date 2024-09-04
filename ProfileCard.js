788460104514e6aa016640654970adb1397026ee
Nielark <markdanielmagpantay2001@gmail.com>
2024-09-04 18:04:07 +0800

    Make responsive web design

--- ProfileCard.js
+++ ProfileCard.js
@@ -1,6 +1,23 @@
-//window.onload = function(){
-    
-    document.getElementById("skills").addEventListener('click', function () {
-        alert("Hello world")
-    })
-//}
+document.addEventListener("DOMContentLoaded", function() {
+    const skillContainer = document.querySelector(".show-skill");
+    const skillToggleBtn = document.querySelector(".skill-toggle-btn");
+
+    var showSkill = false;
+
+    skillToggleBtn.addEventListener("click", () => {
+        showSkill = !showSkill;
+
+        if(showSkill){
+            skillContainer.style.transform = "translateY(0px)";
+            skillToggleBtn.style.top = 0;
+            skillToggleBtn.style.borderRadius = "0 0 20px 20px";
+            skillToggleBtn.style.backgroundColor = "white";
+        }
+        else {
+            skillContainer.style.transform = "translateY(500px)";
+            skillToggleBtn.style.top = "-4%";
+            skillToggleBtn.style.borderRadius = "20px 20px 0 0";
+            skillToggleBtn.style.backgroundColor = "gray";
+        }
+    });
+})
