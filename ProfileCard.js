13cf294463267b1a6ce48ab6f38b89325e1ddac4
Nielark <markdanielmagpantay2001@gmail.com>
2025-03-30 21:23:27 +0800

    Remake the entire profile card

--- ProfileCard.js
+++ ProfileCard.js
@@ -1,23 +1,19 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const skillContainer = document.querySelector(".show-skill");
-    const skillToggleBtn = document.querySelector(".skill-toggle-btn");
-
-    var showSkill = false;
-
-    skillToggleBtn.addEventListener("click", () => {
-        showSkill = !showSkill;
-
-        if(showSkill){
-            skillContainer.style.transform = "translateY(0px)";
-            skillToggleBtn.style.top = 0;
-            skillToggleBtn.style.borderRadius = "0 0 20px 20px";
-            skillToggleBtn.style.backgroundColor = "white";
-        }
-        else {
-            skillContainer.style.transform = "translateY(500px)";
-            skillToggleBtn.style.top = "-4%";
-            skillToggleBtn.style.borderRadius = "20px 20px 0 0";
-            skillToggleBtn.style.backgroundColor = "gray";
-        }
+    //* MODAL DISPLAY
+    document.querySelector(".about-me").addEventListener("click", () => {
+        const modal = document.querySelector(".modal");
+        const overlay = document.querySelector(".overlay");
+    
+        modal.classList.toggle("display-modal");
+        overlay.classList.toggle("display-overlay");
+    });
+    
+    //* MODAL CLOSE
+    document.querySelector(".btn-OK").addEventListener("click", () => {
+        const modal = document.querySelector(".modal");
+        const overlay = document.querySelector(".overlay");
+    
+        modal.classList.toggle("display-modal");
+        overlay.classList.toggle("display-overlay");
     });
 })
